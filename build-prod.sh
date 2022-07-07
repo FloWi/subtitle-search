f149e051a52e3122a1f793be4bf93cada161859d
Florian Witteler <github@flwi.de>
2022-07-07 13:08:08 +0200

    first try of github actions

--- build-prod.sh
+++ build-prod.sh
@@ -7,5 +7,5 @@ mkdir -p docker-dist/webapp
 cp webapp/target/scala-2.13/scalajs-bundler/main/dist/* docker-dist/webapp
 cp file-server/target/scala-2.13/quickstart-assembly-0.0.1-SNAPSHOT.jar docker-dist/
 
-cd docker-dist || exit
-docker build --tag subtitle-search .
+#cd docker-dist || exit
+#docker build --tag subtitle-search .
