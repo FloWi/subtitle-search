c49b139dfcec049d0b7eb4fd90f7e3d76318ee8f
Florian Witteler <github@flwi.de>
2022-07-07 15:11:38 +0200

    fix sharing release artifacts between jobs

--- build-prod.sh
+++ build-prod.sh
@@ -4,8 +4,10 @@ rm docker-dist/*.jar
 rm -r docker-dist/webapp
 
 mkdir -p docker-dist/webapp
+mkdir -p docker-dist/fileserver
+
 cp webapp/target/scala-2.13/scalajs-bundler/main/dist/* docker-dist/webapp
-cp file-server/target/scala-2.13/quickstart-assembly-0.0.1-SNAPSHOT.jar docker-dist/
+cp file-server/target/scala-2.13/quickstart-assembly-0.0.1-SNAPSHOT.jar docker-dist/fileserver
 
 #cd docker-dist || exit
 #docker build --tag subtitle-search .
